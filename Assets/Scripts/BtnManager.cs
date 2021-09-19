a5d79d51933059b85086216aa6d23906dcdd7bb0
rizramadan <rizki123304@gmail.com>
2021-09-19 19:34:25 +0700

    Tower Defense

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour
+{
+    public void BackScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Keluar");
+    }
+}
